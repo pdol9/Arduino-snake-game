576908deeed2880fc25aac28a568e3459bfa5929
pdol <111592903+pdol9@users.noreply.github.com>
2024-03-25 23:14:46 +0100

    add food feature and game speed   - add check for food location and spawning of a new location   - regulate speed of a snake using potentiometer

--- Snake.ino
+++ Snake.ino
@@ -4,18 +4,17 @@
    Playing snake on a LED matrix MAX7219.
 
    The circuit:
-   - red and green LED attached from pin 8 and 9 respectively to ground through 220 ohm resistors
-   - pushbutton (reset button) attached to pin 2 from +5V with 10K resistor attached to pushbutton
+   * red and green LED attached from pin 8 and 9 respectively to ground through 220 ohm resistors
+   * pushbutton (reset button) attached to pin 2 from +5V with 10K resistor attached to pushbutton
    from ground
-   - 10k potentiometer (game's speed controller) attached to A0, with other connectors to ground and 5V
-
-   - LED matrix max7219 8x8
- * VCC: to Arduino 5V
- * GND: to Arduino GND
- * DIN (Data In): to pin 11
- * CLK (Clock): to pin 13
- * CS (Chip Select): to pin 5
- */
+   * 10k potentiometer (game's speed controller) attached to A0, with other connectors to ground and 5V
+   * LED matrix max7219 8x8
+	 - VCC: to Arduino 5V
+	 - GND: to Arduino GND
+	 - DIN (Data In): to pin 11
+	 - CLK (Clock): to pin 13
+	 - CS (Chip Select): to pin 5
+*/
 
 #include <GyverMAX7219.h>
 
@@ -51,27 +50,27 @@ unsigned char snake_position_x = 5;
 unsigned char snake_position_y = 5;
 unsigned char food[2];
 
-inline void  transition(int added_time) {
+inline void  transition(unsigned int added_time) {
 	led.update();
-	delay(100 + added_time);
+	delay(added_time);
 }
 
 void greeting_screen(unsigned int fill) {
 
 	for (int i = 0; i < 8; ++i) {
-		transition(0);
+		transition(100);
 		led.dot(i, 0, fill);
 	}
 	for (int i = 1; i < 8; ++i) {
-		transition(0);
+		transition(100);
 		led.dot(7, i, fill);
 	}
 	for (int i = 6; i >= 0; --i) {
-		transition(0);
+		transition(100);
 		led.dot(i, 7, fill);
 	}
 	for (int i = 6; i > 1; --i) {
-		transition(0);
+		transition(100);
 		led.dot(0, i, fill);
 	}
 	led.dot(1, 2, fill);
@@ -79,25 +78,26 @@ void greeting_screen(unsigned int fill) {
 		transition(2000);
 	/// S letter
 	for (int i = 2; i < 6; ++i) {
-		transition(0);
+		transition(100);
 		led.dot(2, i, fill);
 	}
 	led.dot(3, 5, fill);
-	transition(0);
+	transition(100);
 	for (int i = 5; i > 1; --i) {
 		led.dot(4, i, fill);
-		transition(0);
+		transition(100);
 	}
 	led.dot(5, 2, fill);
-	transition(0);
+	transition(100);
 	for (int i = 2; i < 6; ++i) {
 		led.dot(6, i, fill);
-		transition(0);
+		transition(100);
 	}
 }
 
 void setup() {
 	Serial.begin(9600);
+  randomSeed(analogRead(0));
 
 	// initialize the LED pin as an output:
 	pinMode(red_led, OUTPUT);
@@ -112,49 +112,39 @@ void setup() {
 
 	digitalWrite(red_led, HIGH);
 	digitalWrite(green_led, HIGH);
-	delay(2000);
+	delay(1000);
 	digitalWrite(red_led, LOW);
 
-	int fill = 2;
-
-	// repeat:
-	//   fill -= 1;
-	//   greeting_screen(fill);
-	//   if (fill == 1)
-	//     goto repeat;
-
-	//   led.update();
-	//   delay(3000);
-	//   set_food();
-}
-void  set_food() {
-	food[0] = 4;
-	food[1] = 4;
-	led.dot(food[0], food[1], 1);
+	// run greeting screen
+	for (int clear_greeting_screen = 1; clear_greeting_screen > -1; --clear_greeting_screen) {
+		greeting_screen(clear_greeting_screen);
+	}
 	led.update();
+	delay(1000);
+	update_food();
 }
+
+/// main code
+
+void  food_status() {
+	if (snake_position_x == food[0] && snake_position_y == food[1]) {
+    score_value += 1;
+    update_food();
+  }
+}
+
 void  update_food() {
-	led.dot(food[0], food[1], 0);
-	food[1] = 8 - snake_position_x;
-	food[0] = 8 - snake_position_y;
+  led.dot(food[0], food[1], 0);
+  food[0] = random(0,7);
+  food[1] = random(0,7);
 	led.dot(food[0], food[1], 1);
 	led.update();
 }
 
-int  check_food() {
-	if (snake_position_x == food[0] || snake_position_y == food[1])
-		return 1;
-	return 0;
-}
-
-inline void	update_direction() {
+void	update_direction() {
 
 	int pos_x = analogRead(x_axis);
 	int pos_y = analogRead(y_axis);
-//	Serial.print("pos_x: ");
-//	Serial.println(pos_x / 100);
-//	Serial.print("pos_y: ");
-//	Serial.println(pos_y / 100);
 
 	if (pos_x > 900)
 		current_direction = DOWN;
@@ -166,91 +156,93 @@ inline void	update_direction() {
 		current_direction = RIGHT;
 }
 
-inline void change_direction() {
+void   move(unsigned int game_speed) {
+ 	led.dot(snake_position_x, snake_position_y, 1);
+	transition(200 + game_speed);
+	led.dot(snake_position_x, snake_position_y, 0);
+}
+
+inline void change_direction(unsigned int game_speed) {
 
 	switch (current_direction) {
-		case UP: move_up(); break;
-		case DOWN: move_down(); break;
-		case LEFT: move_left(); break;
-		case RIGHT: move_right(); break;
+		case UP: move_up(game_speed); break;
+		case DOWN: move_down(game_speed); break;
+		case LEFT: move_left(game_speed); break;
+		case RIGHT: move_right(game_speed); break;
 	}
 }
 
-inline void move_up() {
+void move_up(unsigned int game_speed) {
 	if (previous_direction == DOWN) {
-		move_down();					// ignore command of going into opposite direction
+		move_down(game_speed);					// ignore command of going into opposite direction
 		return;
 	}
 	if (snake_position_x == 7)
 		snake_position_x = -1;
 	++snake_position_x;
-	led.dot(snake_position_x, snake_position_y, 1);
-	transition(200);
-	led.dot(snake_position_x, snake_position_y, 0);
+	food_status();
+	move(game_speed);
 	previous_direction = UP;
 }
 
-inline void  move_down() {
+void  move_down(unsigned int game_speed) {
 	if (previous_direction == UP) {
-		move_up();
+		move_up(game_speed);
 		return;
 	}
 	if (snake_position_x == 0)
 		snake_position_x = 8;
 	--snake_position_x;
-	led.dot(snake_position_x, snake_position_y, 1);
-	transition(200);
-	led.dot(snake_position_x, snake_position_y, 0);
+	food_status();
+	move(game_speed);
 	previous_direction = DOWN;
 }
 
-inline void move_right() {
+void move_right(unsigned int game_speed) {
 	if (previous_direction == LEFT) {
-		move_left();
+		move_left(game_speed);
 		return;
 	}
 	if (snake_position_y == 7)
 		snake_position_y = -1;
 	++snake_position_y;
-	led.dot(snake_position_x, snake_position_y, 1);
-	transition(200);
-	led.dot(snake_position_x, snake_position_y, 0);
+	food_status();
+	move(game_speed);
 	previous_direction = RIGHT;
 }
 
-inline void move_left() {
+void move_left(unsigned int game_speed) {
 	if (previous_direction == RIGHT) {
-		move_right();
+		move_right(game_speed);
 		return;
 	}
 	if (snake_position_y == 0)
 		snake_position_y = 8;
 	--snake_position_y;
-	led.dot(snake_position_x, snake_position_y, 1);
-	transition(200);
-	led.dot(snake_position_x, snake_position_y, 0);
+	food_status();
+	move(game_speed);
 	previous_direction = LEFT;
 }
 
 void loop() {
 
+	int reading_speed = analogRead(game_speed_sensor);
 	update_direction();
-	change_direction();
+	change_direction(reading_speed);
 
 	// read the state of the reset button value:
-	int reading_reset = digitalRead(reset_button_score);
-	if (reading_reset == HIGH) {
-		digitalWrite(red_led, HIGH);
-		digitalWrite(green_led, LOW);
-		delay(2000);
-		digitalWrite(red_led, LOW);
-		digitalWrite(green_led, HIGH);
-	}
-	float reading_speed = analogRead(game_speed_sensor) * (5 / 1023.0);
-	// Serial.print("Value of reading speed: ");
-	// Serial.println(reading_speed);
-	// delay(200);
-	int game_speed = reading_speed / 4;
-	// TODO
-	// include value of potentiometer to regulate speed of the snake
+//	int reading_reset = digitalRead(reset_button_score);
+//	if (reading_reset == HIGH) {
+//		digitalWrite(red_led, HIGH);
+//		digitalWrite(green_led, LOW);
+//		delay(2000);
+//		digitalWrite(red_led, LOW);
+//		digitalWrite(green_led, HIGH);
+//	}
 }
+
+	// TODO include value of potentiometer to regulate speed of the snake
+	// TODO generate random spot for food
+	// TODO make snake bigger
+	// TODO snake collision
+	// TODO score reset button
